0606bd42455648517ea0d590f7cbd51bb41087cc
Joseph Livesey <joseph@semiotic.ai>
2024-11-27 18:57:54 -0500

    feat: support firehose client in Rust

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+// Copyright 2024-, Semiotic AI, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! # Firehose in Rust
+//!
+
+mod firehose_v2;
+
+pub(crate) use firehose_v2::single_block_request::BlockNumber;
+
+/// Interact programatically with the Firehose v2 Fetch API.
+pub use firehose_v2::fetch_client::FetchClient;
+
+/// Create Firehose API fetch requests.
+pub use firehose_v2::Request;
+
+/// Work with Firehose API streaming responses.
+pub use firehose_v2::Response;
+
+/// Create Firehose API streaming requests.
+pub use firehose_v2::SingleBlockRequest;
+
+/// Receive Firehose API fetch responses.
+pub use firehose_v2::SingleBlockResponse;
+
+/// Work with the Firehose v2 Stream API.
+pub use firehose_v2::stream_client::StreamClient;
